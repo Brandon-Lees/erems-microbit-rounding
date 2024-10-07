6eb187e14c6a01480446c7f864ec75752b9942bc
Brandon-Lees <wormsfan@gmail.com>
2024-10-07 18:34:07 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,3 @@
-let var1 = 0
-basic.forever(function () {
-    var1 = randint(11, 19)
-    basic.showNumber(var1)
-    if (var1 <= 14) {
-        basic.pause(2000)
-        while (true) {
-            while (input.buttonIsPressed(Button.A)) {
-                var1 = Math.floor(10)
-                basic.showNumber(var1)
-                basic.showIcon(IconNames.Happy)
-                while (false) {
-                    basic.showIcon(IconNames.Sad)
-                }
-            }
-            break;
-        }
-    } else {
-        basic.pause(2000)
-        while (true) {
-            while (input.buttonIsPressed(Button.B)) {
-                var1 = Math.floor(20)
-                basic.showNumber(var1)
-                basic.showIcon(IconNames.Happy)
-                while (false) {
-                    basic.showIcon(IconNames.Sad)
-                }
-            }
-            break;
-        }
-    }
+input.onButtonPressed(Button.A, function () {
+    led.plot(4, 0)
 })
